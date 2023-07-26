7bcac49e06385b25ae095e29db7d32f745f7633d
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-07-26 17:48:08 +0200

    edit

---
+++ README.md
@@ -0,0 +1,66 @@
+# Traccia 1
+
+Chiedi all'utente un primo numero, poi un secondo e genera un numero casuale compreso tra i due.
+
+# Procedimento
+
+- chiedo all'utente un primo numero
+- chiedo all'utente un secondo numero
+
+- genero un numero random tra i due numeri
+
+# Traccia 2
+
+Dare la possibilità di inserire due parole. Verificare che le due parole abbiano la stessa lunghezza. Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
+
+# Procedimento
+
+- chiedo all'utente una parola
+- chiedo all'utente una seconda parola
+- verifico se le due parole hanno la stessa lunghezza
+  - se hanno la stessa lunghezza le stampo entrambe
+    - altrimenti stampo la più lunga
+
+# Traccia 3
+
+Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+
+# Procedimento
+
+- creo un array vuoto
+-
+-
+-
+-
+
+# Traccia 4
+
+Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+Calcola la media dei numeri inseriti.
+
+# Procedimento
+
+-
+-
+-
+-
+-
+-
+-
+
+# Traccia 5
+
+Trovare gli errori nel seguente codice:
+
+---
+
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+    let evenNumbers = "";
+    for (let i = 0; i < numbers.length - 1; i++;) {
+        if (numbers % 2 = 0); {
+            evenNumbers.push(i);
+        }
+    }
+    console.log(evenNumbers);
+
+    // dovrebbe restituire [1,3,5]
--- js/main.js
+++ js/main.js
@@ -1,1 +1,71 @@
+// // Es 1
 
+// // chiedo all'utente un primo numero
+// const userFirstNumber = parseInt(prompt("Digita un numero"));
+// console.log(userFirstNumber);
+// // chiedo all'utente un secondo numero
+// const userSecondNumber = parseInt(prompt("Digita un secondo numero"));
+// console.log(userSecondNumber);
+
+// // genero un numero random tra i due numeri
+// const randomNumber =
+//   Math.floor(Math.random() * (userFirstNumber - userSecondNumber)) +
+//   userSecondNumber;
+// console.log(randomNumber);
+
+// // Es 2
+
+// // chiedo all'utente una parola
+// const firstWord = prompt("Inserisci una parola");
+// console.log(firstWord);
+
+// // chiedo all'utente una seconda parola
+// const secondWord = prompt("Inserisci una seconda parola");
+// console.log(secondWord);
+
+// // verifico se le due parole hanno la stessa lunghezza
+// if (firstWord.length == secondWord.length) {
+//   //se hanno la stessa lunghezza le stampo entrambe
+//   alert(firstWord + " " + secondWord);
+//   // altrimenti stampo la più lunga
+// } else if (firstWord.length > secondWord.length) {
+//   alert(firstWord);
+// } else {
+//   alert(secondWord);
+// }
+
+// // Es 3
+
+// // creo un array vuoto
+// const numbersContainer = [];
+
+// // creo la variabile per contenere la somma
+// let sum = 0;
+
+// // continuo a chiedere i numeri all'utente finchè la somma è 50
+// while (sum < 50) {
+//   let userNumber = parseInt(prompt("Digita un numero"));
+//   sum += userNumber;
+
+//   if (sum < 50) {
+//     numbersContainer.push(userNumber);
+//   }
+// }
+
+// console.log(numbersContainer);
+
+// Es 4
+
+// // Es 5
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// let evenNumbers = [];
+
+// for (let i = 0; i < numbers.length - 1; i++) {
+//   if (numbers % 2 == 0) {
+//     evenNumbers.push(i);
+//   }
+// }
+
+// console.log(evenNumbers);
